8fd68baa8ed2adaec3c2bafffa8ca9420f955dac
Caleb Houlding <dev@calebhoulding.com>
2024-10-31 21:54:24 +0000

    1.1.0 The Pretty Update
    - Overhauled the UI.
    - Added a colour theme.
    - Re-organised the UI to make more sense.
    - Added an icon and title image.
    - Added a custom font.

--- obj/Release/net8.0/win-x64/Isembard.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/Isembard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Isembard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d30e4237f230f5538ee87eedee3456a6fcf97ed1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Isembard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Isembard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
